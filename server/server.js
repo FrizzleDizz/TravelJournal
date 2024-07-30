fb7acb1ecf1d3c046743fb0043b0d31bffeb7791
ZaynabLunat <lunat815@gmail.com>
2024-07-30 10:03:44 +0100

    images!

--- server/server.js
+++ server/server.js
@@ -14,14 +14,28 @@ app.get("/", function (request, response) {
 //     response.json(images);
 //   });
 
-const img1 =
-  "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg";
+const images = [
+  "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg",
+  "https://www.sandals.co.uk/blog/content/images/2022/06/Header-_-1-Blue-Hole-Jamaica-1.jpg",
+  "https://earth.esa.int/web/earth-watching/content/documents/257246/1608677/Barcelona.jpg",
+];
 
-const img2 =
-  "https://www.sandals.co.uk/blog/content/images/2022/06/Header-_-1-Blue-Hole-Jamaica-1.jpg";
+// const img1 =
+//   "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg";
 
-const img3 =
-  "https://www.state.gov/wp-content/uploads/2023/07/shutterstock_1124332706v2.jpggit";
+// const img2 =
+//   "https://www.sandals.co.uk/blog/content/images/2022/06/Header-_-1-Blue-Hole-Jamaica-1.jpg";
+
+// const img3 =
+//   "https://www.state.gov/wp-content/uploads/2023/07/shutterstock_1124332706v2.jpggit";
+
+app.get("/", function (request, response) {
+  response.json("Hello?");
+});
+
+app.get("/images", function (request, response) {
+  response.json(images);
+});
 
 app.listen(8080, function () {
   console.log("Listening to port 8080");
