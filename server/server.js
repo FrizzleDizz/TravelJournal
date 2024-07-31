5bbe295a00d2e7cde38cf9393949f7b66a3ca1d0
ZaynabLunat <lunat815@gmail.com>
2024-07-31 14:47:13 +0100

    changes

--- server/server.js
+++ server/server.js
@@ -10,32 +10,24 @@ app.use(express.json()); // this line lets us use the "body" from the request
 
 const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-app.get("/", function (request, response) {
-  response.json("Hello?");
-});
-
-app.get("/images", function (request, response) {
-  response.json(images);
-});
-
 const images = [
   "https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/02/a0002487/img/basic/a0002487_main.jpg",
   "https://www.sandals.co.uk/blog/content/images/2022/06/Header-_-1-Blue-Hole-Jamaica-1.jpg",
   "https://earth.esa.int/web/earth-watching/content/documents/257246/1608677/Barcelona.jpg",
 ];
 
-app.get("/images", function (request, response) {
-  response.json(images);
-});
-
 app.get("/", function (request, response) {
   response.json("Hello?");
+});
+
+app.get("/images", function (request, response) {
+  response.json(images);
 });
 
 // message get
 app.get("/message", async function (request, response) {
   // get the existing messages from the database
-  const result = await db.query(`SELECT * FROM travel`); 
+  const result = await db.query(`SELECT * FROM travel`);
   const messages = result.rows;
   response.json(messages);
 });
